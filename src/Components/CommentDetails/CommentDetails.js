90f1b15465fa1ba11684f41e7cf37b20c48c531c
md jahidul islam <za0162hid@gmail.com>
2020-12-06 17:35:12 +0600

    comments and picture added

--- src/Components/CommentDetails/CommentDetails.js
+++ src/Components/CommentDetails/CommentDetails.js
@@ -1,16 +1,18 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import '../CommentDetails/CommentDetails.css';
 
 const CommentDetails = () => {
   const {postId} = useParams();
  
   const [comments, setComments] = useState([])
-  console.log(comments);
-  const [post, setPost] = useState({})
-  const [photo, setPhoto] = useState([])
-  console.log(post);
+  // console.log(comments);
+ 
+  const [image, setImage] = useState([])
+ 
 //   console.log(comments);
   useEffect(()=>{
     fetch(`https://jsonplaceholder.typicode.com/comments/?postId=${postId}`) 
@@ -18,20 +20,51 @@ const CommentDetails = () => {
     .then(data => setComments(data))
 
     
-    fetch(`https://jsonplaceholder.typicode.com/photos/${postId}`)
+    fetch("https://randomuser.me/api/?results=10")
     .then(res => res.json())
-    .then(data => setPhoto(data))
-  },[])                     
-return (
-<div>
-    
+    .then(data => setImage(data.results)) //jehetu image ghula shudhu pathabo ar ata loop akare jabe sehetu ar index[0/1/2] set korinay
+  },[])
 
-    {
-     comments.map(cm => <h1>{cm.name}</h1> )
-    }
-    <h1>Photo title: {photo.title}</h1> 
-    <img src={photo.thumbnailUrl} alt=""/>                
+  let pictures = [];
+  for (let i = 0; i < image.length; i++) {
+    const item = image[i];
+    const pics = item.picture
+    const largePicture = pics.large
+    pictures.push(largePicture) 
+}
+
+return (
+  <div style={{margin: '30px'}}>
+  <h1> Comments:</h1>
+
+  {
+      comments.map((comment,idx) => //akhne useState ar index number ek sathe pathano hoise ar index number o pathano hoise..
+      
+          <div>
+              <div  className="comment__top">
+                  <Avatar alt="MIF" src={pictures[idx]} />
+                  <h3> {comment.name} </h3>
+              </div>
+              <p style={{color:"#880e4f"}}> Post Id: {comment.postId} </p>
+              <p> Email: {comment.email} </p>
+              <p>{comment.body}</p>
+          </div>
+      )
+  }
 </div>
+    // <div>
+    //   <h1>comments</h1>
+    //   {
+    //       comments.map((comments, idx)=>
+    //       <div>
+    //         <div>
+    //         <Avatar alt="" src={pictures}>
+    //         </div>
+    //       </div>
+    //       )
+    //   }
+
+    // </div>
 );
 };
 
